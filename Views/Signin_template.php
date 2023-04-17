b1a7da8f7ab81e05e2fc7467c36f027ab78e5ce0
root <m.benattou@live.fr>
2023-04-17 01:40:13 +0000

    modif couleur label

--- Views/Signin_template.php
+++ Views/Signin_template.php
@@ -1,22 +1,22 @@
 <?php
 include "Header_template.php" ;
 ?>
+<div class="main">
+    <form id="sign" method="POST" action="Signin/signin">
+        <label for="firstname">First Name : </label>
+        <input type="text" id="firstname" name="firstname" placeholder="First Name" required autofocus>
+        <label for="lastname">Last Name : </label>
+        <input type="text" id="lastname" name="lastname" placeholder="Last Name" required>
+        <label for="nickname">Nickname : </label>
+        <input type="text" id="nickname" name="nickname" placeholder="Nickname" required>
+        <label for="email">Email : </label>
+        <input type="email" id="email" name="mail" placeholder="Email" required>
+        <label for="password">Password : </label>
+        <input type="password" id="password" name="password" placeholder="Password" required>
 
-<form id="sign" method="POST" action="Signin/signin">
-    <label for="firstname">First Name : </label>
-    <input type="text" id="firstname" name="firstname" placeholder="First Name" required autofocus>
-    <label for="lastname">Last Name : </label>
-    <input type="text" id="lastname" name="lastname" placeholder="Last Name" required>
-    <label for="nickname">Nickname : </label>
-    <input type="text" id="nickname" name="nickname" placeholder="Nickname" required>
-    <label for="email">Email : </label>
-    <input type="email" id="email" name="mail" placeholder="Email" required>
-    <label for="password">Password : </label>
-    <input type="password" id="password" name="password" placeholder="Password" required>
-
-    <button class="button">Sign In</button>
-</form>
-
+        <button class="button">Sign In</button>
+    </form>
+</div>
 <?php
 include "Footer_template.php" ;
 ?>
