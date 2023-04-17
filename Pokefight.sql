61bd2a7b260322e75a3b6ee36315dcb31015af7c
root <m.benattou@live.fr>
2023-04-17 02:39:15 +0000

    ajout de nouveaux pokemons

--- Pokefight.sql
+++ Pokefight.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : dim. 16 avr. 2023 à 19:11
+-- Généré le : lun. 17 avr. 2023 à 02:37
 -- Version du serveur : 8.0.32
 -- Version de PHP : 8.1.15
 
@@ -52,7 +52,6 @@ INSERT INTO `Combat` (`id_combat`, `user`, `my_poke1`, `my_poke2`, `poke_ia1`, `
 (51, 30, 'noeunoeuf', 'ptitard', 'otaria', 'ponyta', 26, 3, '16-04-2023 17:31:16', 'victoire'),
 (52, 30, 'noeunoeuf', 'ptitard', 'otaria', 'ponyta', 18, 3, '16-04-2023 17:31:19', 'victoire'),
 (53, 30, 'noeunoeuf', 'ptitard', 'otaria', 'ponyta', 18, 3, '16-04-2023 17:31:25', 'victoire'),
-(54, 21, 'noeunoeuf', 'otaria', 'voltorbe', 'ponyta', 21, 0, '16-04-2023 18:18:23', 'défaite'),
 (55, 32, 'pikachu', 'raichu', 'voltorbe', 'electrode', 37, 0, '16-04-2023 18:52:25', 'défaite'),
 (56, 32, 'pikachu', 'raichu', 'voltorbe', 'electrode', 37, 3, '16-04-2023 18:52:32', 'victoire'),
 (57, 32, 'pikachu', 'raichu', 'voltorbe', 'electrode', 37, 3, '16-04-2023 18:52:35', 'victoire'),
@@ -69,7 +68,48 @@ INSERT INTO `Combat` (`id_combat`, `user`, `my_poke1`, `my_poke2`, `poke_ia1`, `
 (68, 30, 'noeunoeuf', 'voltorbe', 'bulbizare', 'otaria', 28, 3, '16-04-2023 19:10:49', 'victoire'),
 (69, 30, 'bulbizare', 'ponyta', 'caninos', 'psykokwak', 25, 0, '16-04-2023 19:12:52', 'défaite'),
 (70, 30, 'bulbizare', 'ponyta', 'caninos', 'psykokwak', 31, 0, '16-04-2023 19:12:55', 'défaite'),
-(71, 30, 'bulbizare', 'ponyta', 'caninos', 'psykokwak', 18, 0, '16-04-2023 19:13:01', 'défaite');
+(71, 30, 'bulbizare', 'ponyta', 'caninos', 'psykokwak', 18, 0, '16-04-2023 19:13:01', 'défaite'),
+(72, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:09', 'victoire'),
+(73, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:15', 'victoire'),
+(74, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:17', 'victoire'),
+(75, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:21', 'victoire'),
+(76, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:26', 'victoire'),
+(77, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:29', 'victoire'),
+(78, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:16:33', 'victoire'),
+(79, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:17:08', 'victoire'),
+(80, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 9, 3, '16-04-2023 22:17:13', 'victoire'),
+(81, 33, 'voltorbe', 'electrode', 'akwakwak', 'lamantine', 10, 3, '16-04-2023 22:17:17', 'victoire'),
+(82, 33, 'voltorbe', 'ponyta', 'otaria', 'tetarte', 17, 0, '16-04-2023 22:39:24', 'défaite'),
+(83, 33, 'lamantine', 'electrode', 'raichu', 'arcanin', 20, 3, '16-04-2023 22:39:42', 'victoire'),
+(84, 33, 'lamantine', 'electrode', 'raichu', 'arcanin', 16, 3, '16-04-2023 22:39:48', 'victoire'),
+(85, 33, 'lamantine', 'electrode', 'raichu', 'arcanin', 16, 3, '16-04-2023 22:40:17', 'victoire'),
+(86, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 9, 3, '17-04-2023 02:45:49', 'victoire'),
+(87, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 10, 3, '17-04-2023 02:46:46', 'victoire'),
+(88, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 10, 3, '17-04-2023 02:46:52', 'victoire'),
+(89, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 9, 3, '17-04-2023 02:46:56', 'victoire'),
+(90, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 9, 3, '17-04-2023 02:47:03', 'victoire'),
+(91, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 9, 3, '17-04-2023 02:47:12', 'victoire'),
+(92, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 9, 3, '17-04-2023 02:47:15', 'victoire'),
+(93, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 9, 3, '17-04-2023 02:47:18', 'victoire'),
+(94, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 10, 3, '17-04-2023 02:47:21', 'victoire'),
+(95, 34, 'pikachu', 'raichu', 'carapuce', 'tetarte', 10, 3, '17-04-2023 02:47:25', 'victoire'),
+(96, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 10, 3, '17-04-2023 02:52:56', 'victoire'),
+(97, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 9, 3, '17-04-2023 02:53:02', 'victoire'),
+(98, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 9, 3, '17-04-2023 02:53:05', 'victoire'),
+(99, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 9, 3, '17-04-2023 02:53:08', 'victoire'),
+(100, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 10, 3, '17-04-2023 02:53:12', 'victoire'),
+(101, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 10, 3, '17-04-2023 02:53:15', 'victoire'),
+(102, 34, 'pikachu', 'raichu', 'akwakwak', 'lamantine', 10, 3, '17-04-2023 02:53:20', 'victoire'),
+(103, 36, 'ponyta', 'feunard', 'bulbizare', 'tetarte', 16, 0, '17-04-2023 03:50:04', 'défaite'),
+(104, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:50:56', 'victoire'),
+(105, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:02', 'victoire'),
+(106, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:05', 'victoire'),
+(107, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:10', 'victoire'),
+(108, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:13', 'victoire'),
+(109, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:21', 'victoire'),
+(110, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:24', 'victoire'),
+(111, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:27', 'victoire'),
+(112, 36, 'ponyta', 'feunard', 'bulbizare', 'noeunoeuf', 9, 3, '17-04-2023 03:51:33', 'victoire');
 
 -- --------------------------------------------------------
 
@@ -114,7 +154,19 @@ INSERT INTO `Pokemon` (`id_pokemon`, `nom`, `PV`, `PVMax`, `PC`, `Type`, `Nb_pot
 (24, 'voltorbe', 200, 200, 10, 'electro', 1, 'https://www.pokepedia.fr/images/thumb/5/59/Voltorbe-RFVF.png/375px-Voltorbe-RFVF.png'),
 (25, 'electrode', 200, 200, 15, 'electro', 1, 'https://www.pokepedia.fr/images/thumb/0/03/%C3%89lectrode-RFVF.png/375px-%C3%89lectrode-RFVF.png'),
 (26, 'noeunoeuf', 200, 200, 10, 'plante', 1, 'https://www.pokepedia.fr/images/thumb/3/39/Noeunoeuf-RFVF.png/375px-Noeunoeuf-RFVF.png'),
-(27, 'noadkoko', 200, 200, 12, 'plante', 1, 'https://www.pokepedia.fr/images/thumb/1/1f/Noadkoko-RFVF.png/375px-Noadkoko-RFVF.png');
+(27, 'noadkoko', 200, 200, 12, 'plante', 1, 'https://www.pokepedia.fr/images/thumb/1/1f/Noadkoko-RFVF.png/375px-Noadkoko-RFVF.png'),
+(28, 'Mewtwo', 200, 200, 20, 'electro', 2, 'https://www.pokepedia.fr/images/thumb/d/d8/Mewtwo-DEPS.png/375px-Mewtwo-DEPS.png'),
+(29, 'Mew', 200, 200, 20, 'electro', 2, 'https://www.pokepedia.fr/images/thumb/1/16/Mew-DEPS.png/375px-Mew-DEPS.png'),
+(30, 'Electhor', 200, 200, 15, 'electro', 1, 'https://www.pokepedia.fr/images/thumb/8/81/%C3%89lecthor-RFVF.png/375px-%C3%89lecthor-RFVF.png'),
+(31, 'Artikodin', 200, 200, 15, 'eau', 1, 'https://www.pokepedia.fr/images/thumb/b/bb/Artikodin-RFVF.png/375px-Artikodin-RFVF.png'),
+(32, 'Sulfura', 200, 200, 15, 'feu', 1, 'https://www.pokepedia.fr/images/thumb/d/db/Sulfura-DEPS.png/375px-Sulfura-DEPS.png'),
+(33, 'Magicarpe', 200, 200, 5, 'eau', 5, 'https://www.pokepedia.fr/images/thumb/0/06/Magicarpe-RFVF.png/375px-Magicarpe-RFVF.png'),
+(34, 'Magneti', 200, 200, 10, 'electro', 1, 'https://www.pokepedia.fr/images/thumb/b/b9/Magn%C3%A9ti-RFVF.png/375px-Magn%C3%A9ti-RFVF.png'),
+(35, 'Magneton', 200, 200, 15, 'electro', 1, 'https://www.pokepedia.fr/images/thumb/3/36/Magn%C3%A9ton-RFVF.png/375px-Magn%C3%A9ton-RFVF.png'),
+(36, 'Miaouss', 200, 200, 8, 'electro', 3, 'https://www.pokepedia.fr/images/thumb/b/b8/Miaouss-RFVF.png/375px-Miaouss-RFVF.png'),
+(37, 'Tortank', 200, 200, 15, 'eau', 1, 'https://www.pokepedia.fr/images/thumb/2/24/Tortank-RFVF.png/375px-Tortank-RFVF.png'),
+(38, 'Dracaufeu', 200, 200, 15, 'feu', 1, 'https://www.pokepedia.fr/images/thumb/1/17/Dracaufeu-RFVF.png/375px-Dracaufeu-RFVF.png'),
+(39, 'Florizare', 200, 200, 15, 'plante', 1, 'https://www.pokepedia.fr/images/thumb/4/42/Florizarre-RFVF.png/375px-Florizarre-RFVF.png');
 
 -- --------------------------------------------------------
 
@@ -137,14 +189,17 @@ CREATE TABLE `User` (
 
 INSERT INTO `User` (`id_user`, `firstname`, `lastname`, `nickname`, `mail`, `password`) VALUES
 (20, 'q', 'q', 'q', 'q@q', '8e35c2cd3bf6641bdb0e2050b76932cbb2e6034a0ddacc1d9bea82a6ba57f7cf'),
-(21, 'w', 'w', 'w', 'w@w', '50e721e49c013f00c62cf59f2163542a9d8df02464efeb615d31051b0fddc326'),
 (22, 'moi', 'moi', 'moi', 'moi@moi', 'a4f0e1d1b5eb23e3482a14b4a9d4e8106e83f7887471e4a73c2557ade280bfe5'),
 (23, 'a', 'a', 'a', 'a@a', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'),
 (24, 'azerty', 'azerty', 'azerty', 'azerty@azerty', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9'),
 (29, 'aze', 'aze', 'aze', 'w@w', 'd4b3dfbf113cc8b2f6fd71bcb24b761d04b47c04a59b22a2a7db91b275542892'),
 (30, 'michaël', 'benattou', 'mika', 'm.ben@liv.fr', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9'),
 (31, 'b', 'b', 'b', 'b@b', '3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d'),
-(32, 'd', 'd', 'd', 'd@d', '18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4');
+(32, 'd', 'd', 'd', 'd@d', '18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4'),
+(33, 'Didier', 'Bourdon', 'didier', 'dd@dd.fr', '9b7ecc6eeb83abf9ade10fe38865df4499be3568dcc507ae2ec3b44989cb0093'),
+(34, 'Satochi', 'Bourg-palette', 'Sacha', 'Sacha@pokemon.fr', '5982776464b2533a0bd175fd4ec4bdd5540a6552c50e0e7501a5109b709cf6c1'),
+(35, 'albert', 'albert', 'albert', 'albert@cimer.com', '5fc6171f625ef967eba2588cd40e85479d7980690b4d7a214e6e56757ba33a20'),
+(36, 'diane', 'robert', 'diane', 'd@r', 'a6b988796bd91c209657d13fc7c14b8a7c9652cec0232f97046adb2e916ed5cc');
 
 --
 -- Index pour les tables déchargées
@@ -177,19 +232,19 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT pour la table `Combat`
 --
 ALTER TABLE `Combat`
-  MODIFY `id_combat` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id_combat` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT pour la table `Pokemon`
 --
 ALTER TABLE `Pokemon`
-  MODIFY `id_pokemon` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_pokemon` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT pour la table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Contraintes pour les tables déchargées
